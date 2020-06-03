64f34f1bff09e35d275bc399a7d2603dd4fdbf2e
Hein <hgrobler1214@gmail.com>
2020-06-03 16:10:48 +1000

    Portals Working
    portals are now working properly and can work in any scene without and dependencies

---
+++ Assets/Hein/HeinPortal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeinPortal : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField]
+    private GameObject portalOut;
+    [SerializeField]
+    private bool flipXSpeed = false;
+    [SerializeField]
+    private bool flipYSpeed = false;
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        rb = collider.GetComponent<Rigidbody2D>();
+
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.transform.position = portalOut.transform.position;
+
+            if (flipXSpeed)
+            {
+                rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+            }
+            else if(flipYSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+            }
+        }
+    }
+}
