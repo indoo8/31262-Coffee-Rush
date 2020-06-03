5fe8e6edd56d03943e4b77e938a0d167e5ba1e00
Hein <hgrobler1214@gmail.com>
2020-06-03 14:28:40 +1000

    Initial scene setup
    HeinScene is now in project
    Everyone else swap over your fonts to the new on in shared
    (you'll also have to change your font sizes to the ones in my scene)

---
+++ Assets/Hein/HeinGameManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeinGameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player, deathMessage, pauseMenu;
+    [SerializeField] private Transform playerStartPos;
+    [SerializeField] private GameObject[] coffeeBeans;
+    [SerializeField] private RisingWater risingWater;
+    [SerializeField] private Animator pAnimator, cmAnimator;
+    private HeinInputManager iManager;
+    private int beanCount = 0;
+    public bool paused = false, dead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        iManager = gameObject.GetComponent<HeinInputManager>();
+        player.transform.position = playerStartPos.position;
+        deathMessage.SetActive(false);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Respawn()
+    {
+        pAnimator.SetTrigger("respawn");
+        player.transform.position = playerStartPos.position;
+        iManager.UnFreeze();
+        deathMessage.SetActive(false);
+        pauseMenu.SetActive(false);
+        dead = false;
+        Time.timeScale = 1;
+        for (int i=0; i<coffeeBeans.Length; i++)
+        {
+            coffeeBeans[i].SetActive(true);
+        }
+    }
+
+    public void PlayerDie()
+    {
+        pAnimator.SetTrigger("dead");
+        iManager.Freeze();
+        deathMessage.SetActive(true);
+        dead = true;
+    }
+
+    public void CollectBean()
+    {
+        beanCount++;
+        Debug.Log("beanCount = " + beanCount);
+    }
+
+    public void ReachCoffeeMachine()
+    {
+        if (beanCount == 3)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        iManager.Freeze();
+        Time.timeScale = 0;
+        paused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        iManager.UnFreeze();
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        //UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+
+    }
+}
---
+++ Assets/Hein/HeinPlayerCollisions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeinPlayerCollisions : MonoBehaviour
+{
+    public HeinGameManager gManager;
+
+    private void Start()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "death")
+        {
+            Debug.Log("collided with " + collision.gameObject.name);
+            gManager.PlayerDie();
+        }
+        if (collision.gameObject.tag == "bean")
+        {
+            gManager.CollectBean();
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
