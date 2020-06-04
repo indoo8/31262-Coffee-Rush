d79b0ee9a7bab7ebf140f0d4c205f2a9e533062b
indoo8 <65959029+indoo8@users.noreply.github.com>
2020-06-04 19:57:37 +1000

    first finished tutorial level
    finished first version of tutorial level with start/end cutscenes and end level menu that counts how many beans you collected

---
+++ Assets/TutorialLevel/TutorialEndCutsceneManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEndCutsceneManager : MonoBehaviour
+{
+    [SerializeField] private GameObject cBandit;
+    [SerializeField] private Transform banditStartPos, banditEndPos;
+    private Animator bAnimator;
+    [SerializeField] private Animator tAnimator;
+    private Transform cBanditTransform;
+    [SerializeField] private TutorialGameManager gManager;
+    [SerializeField] private float runSpeed;
+    private bool banditRun = false;
+    [SerializeField] GameObject foundText, escapingText, beginningText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cBanditTransform.position = banditStartPos.transform.position;
+        bAnimator = cBandit.GetComponent<Animator>();
+        tAnimator.SetBool("canEnter", true);
+        cBanditTransform = cBandit.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (banditRun)
+        {
+            cBanditTransform.Translate(Vector3.left * runSpeed * Time.deltaTime);
+            bAnimator.SetFloat("speed", runSpeed);
+            /*
+            if (cBanditTransform.position.x <= banditEndPos.position.x)
+            {
+                banditRun = false;
+                bAnimator.SetFloat("speed", 0);
+         
+            }*/
+
+        }
+    }
+
+    public void StartEndCutscene()
+    {
+        Debug.Log("startEndCutscene");
+        StartCoroutine(EndCutscene());
+    }
+
+    IEnumerator EndCutscene()
+    {
+        Debug.Log("endCutsceneStarted");
+        foundText.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        foundText.SetActive(false);
+        escapingText.SetActive(true);
+        banditRun = true;
+        yield return new WaitForSeconds(3f);
+        escapingText.SetActive(false);
+        beginningText.SetActive(true);
+        cBandit.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        gManager.EndLevelScreen();
+
+    }
+}
