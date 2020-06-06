93fb381d74898f89a200c90eee37cae750be8cda
GavinW015 <66008911+GavinW015@users.noreply.github.com>
2020-06-06 17:40:44 +1000

    er

---
+++ Assets/Gavin/enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public int health = 100;
+
+    public GameObject deathEffect;
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die ()
+    {
+        Instantate(deathEffect, transform.position, Quaternion.identify);
+        Destroy(gameObject);
+    }
+
+}
