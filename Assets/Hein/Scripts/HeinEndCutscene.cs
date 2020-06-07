c202e981230aa804dd45189be085d7df6f5e65ea
Hein <hgrobler1214@gmail.com>
2020-06-07 23:34:48 +1000

    Level is pretty much finished

---
+++ Assets/Hein/Scripts/HeinEndCutscene.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeinEndCutscene : MonoBehaviour
+{
+    [SerializeField] private GameObject cBandit;
+    [SerializeField] private HeinGameManager gManager;
+    private Animator cBanditAnimator;
+    private Transform cBanditTransform;
+    [SerializeField] private float banditSpeed;
+    private bool banditRun = false;
+
+    void Start()
+    {
+        cBanditAnimator = cBandit.GetComponent<Animator>();
+        cBanditTransform = cBandit.transform;
+    }
+
+    void Update()
+    {
+        if (banditRun)
+        {
+            BanditRun();
+        }
+    }
+
+    public void BanditRun()
+    {
+        cBanditTransform.Translate(Vector3.right * banditSpeed * Time.deltaTime);
+        cBanditAnimator.SetFloat("speed", banditSpeed);
+    }
+    
+
+    IEnumerator StartCutscene()
+    {
+        yield return new WaitForSeconds(0.71f);
+        Destroy(cBandit);
+        banditRun = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("collied with animation box");
+            banditRun = true;
+            StartCoroutine(StartCutscene());
+        }
+    }
+}
--- Assets/Hein/Scripts/HeinEndLevel.cs
+++ Assets/Hein/Scripts/HeinEndLevel.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeinCoffeeMachine : MonoBehaviour
+public class HeinEndLevel : MonoBehaviour
 {
     [SerializeField] private HeinGameManager gManager;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
+        Time.timeScale = 0;
         gManager.EndLevelScreen();
     }
 }
