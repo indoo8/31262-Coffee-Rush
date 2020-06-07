8dec4911a510071e9de689e470f3731be890b909
Hein <hgrobler1214@gmail.com>
2020-06-07 15:34:58 +1000

    Bunch of progress
    Checkpoints
    Animated bean bandit at beginning
    portals styles changed
    sign updates
    difficulty adjustments

---
+++ Assets/Hein/HeinCheckpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeinCheckpoint : MonoBehaviour
+{
+    public GameObject spawn;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            spawn.transform.position = this.transform.position;
+        }
+
+    }
+}
